7fc0ef3f2a7f9cf4fa55c69810ca6dfc13058c04
randyaajr <67880000+randyaajr@users.noreply.github.com>
2020-11-08 23:50:09 -0500

    Updated assignment

--- py-assignment1.py
+++ py-assignment1.py
@@ -3,7 +3,8 @@ num_of_courses = int(input('How many courses did you finish?'))
 
 course_marks = []
 
-counter = 0
-while num_of_courses in  range(5):
-    print(f'Enter your mark for class')
-    break
+while num_of_courses:
+    input('Enter your mark for course')
+
+    if num_of_courses == "":
+       break
