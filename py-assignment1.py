487cd3eb946513abdf11b5353ee7a8ddb2d3cef2
randyaajr <67880000+randyaajr@users.noreply.github.com>
2020-11-09 00:30:31 -0500

    Bug update

--- py-assignment1.py
+++ py-assignment1.py
@@ -4,8 +4,9 @@ num_of_courses = int(input('How many courses did you finish?'))
 course_marks = []
 
 count = 1
-while num_of_courses < 6:
-    int(input(f'Enter your mark for course' + course_marks))
+while count < num_of_courses:
+    course_marks.append(int(input(f'Enter your marks for course {str(count)}:')))
+    count += 1
 
     if num_of_courses == " ":
        break
