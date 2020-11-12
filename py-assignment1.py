2df4b1f88b6a4ae181fbb58d16b6af09d8f2bd76
randyaajr <67880000+randyaajr@users.noreply.github.com>
2020-11-12 14:27:51 -0500

    The app now finds the average of all marks entered

--- py-assignment1.py
+++ py-assignment1.py
@@ -11,3 +11,10 @@ while (count <= num_of_courses):
 
 for item in course_marks:
     print(item)
+
+total = 0
+for mark in course_marks:
+    total = total + mark
+
+average = round(total/num_of_courses, 2)
+print(f'Ypur average for your {num_of_courses} courses in {average}')
