346b5858bb3ccd0064e43c7c177ce794771dc56b
randyaajr <67880000+randyaajr@users.noreply.github.com>
2020-11-12 15:06:48 -0500

    Assignment completed

--- py-assignment1.py
+++ py-assignment1.py
@@ -1,20 +1,40 @@
+# Variable declared whick asks for user input and stores the value entered.
 num_of_courses = int(input('How many courses did you finish?'))
 
-
+# This empty variable store the values entered by the user.
 course_marks = []
 
+# This while loop iterates through the inputed amount of courses completed.
 count = 1
 while (count <= num_of_courses):
     course_marks.append(
         int(input(f'Enter your marks for course {str(count)}:')))
     count += 1
 
+# The for loop here then prints (to the terminal) the inputed values.
 for item in course_marks:
     print(item)
 
-total = 0
+# Here is where the values are added. 
+continuer = 0
 for mark in course_marks:
-    total = total + mark
+    continuer = continuer + mark
 
-average = round(total/num_of_courses, 2)
-print(f'Ypur average for your {num_of_courses} courses in {average}')
+# Now continuer is devided with the number of courses entered.
+average = round(continuer / num_of_courses, 1)
+print(f'Your average for your {num_of_courses} courses is: {average}')
+
+# Last but not least, the if statment which determines the overall alphabetical grade.
+if average >= 90 and average <= 100:
+    print('Your grade is A')
+elif average >= 80 and average <= 89:
+   print('Your grade is B')
+elif average >= 70 and average <= 79:
+    print('Your grade is C')
+elif average >= 60 and average <= 69:
+    print('your grade is D')
+else:
+    print('Your grade is F')
+
+
+# End of loop
